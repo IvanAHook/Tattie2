81ebcdaed0b49424cc87360d08c56ce779276b29
unknown <kalle.nystrom@WH931623.CM.lan>
2015-06-17 19:53:19 +0200

    Början på grisAI

---
+++ Assets/Scripts/AI/Pig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pig : MonoBehaviour {
+
+	public Animator anim;
+
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			anim.SetTrigger("Ass");
+		}
+		
+		
+		
+	}
+}
